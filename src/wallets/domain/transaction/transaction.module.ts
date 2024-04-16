229f1fb9110551b832cf6f60c0bbd6ec98273cc8
alexagep <68432637+alexagep@users.noreply.github.com>
2024-04-16 14:50:29 +0330

    Revert "Feature/grpc implement"

--- src/wallets/domain/transaction/transaction.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TransactionRepository } from './transaction.repository';
-
-@Module({
-  providers: [TransactionRepository],
-  exports: [TransactionRepository],
-})
-export class TransactionModule {}
--- src/wallets/dto/wallet-transaction-response-dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class TransactionResponseDto {
-  @ApiProperty({
-    description: 'Message indicating the success or failure of the transaction',
-    example: 'Transaction successful',
-  })
-  reference_id: string;
-}
